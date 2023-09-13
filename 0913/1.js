15c50e9cfc16c6ffe262b828caf3c818e9ef11f0
nayoung3669 <skdud3669@gmail.com>
2023-09-13 16:43:24 +0900

    init

---
+++ 0913/1.js
@@ -0,0 +1,4 @@
+var a = 12;
+var b = null //null이 객체..?
+typeof(a)
+typeof(b) //object
---
+++ 0913/2.js
@@ -0,0 +1,24 @@
+let start = 0
+let stop = 10
+
+// function iterateover (start, stop) {
+//     for (i = start; i < stop; i++) {
+//         console.log("I am number", i)
+//     }
+// }
+
+//anonymous function
+let iterateover = function (start, stop) {
+    //...
+}
+
+iterateover(0.10)
+console.log(typeof(iterateover)) // results will be "function"
+
+let stds = ["std1", "std2", "std3"]
+console.log(stds.indexOf("std5")) // results will be -1
+//if (stds.indexOf(sthm") === -1) { ... } 조건문으로 쓸 수 있음. 있는지 없는지 검사
+
+
+var cars = ["a","b","c","i"]
+console.log(cars.push("n")) // return 5
