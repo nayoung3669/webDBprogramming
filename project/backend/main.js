cd1db7faa834fe7c3921479149b98bf4d144f38a
nayoung3669 <skdud3669@gmail.com>
2023-11-10 16:48:37 +0900

    get function

--- project/backend/main.js
+++ project/backend/main.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 app.use(cors());
 
 // sql
@@ -30,22 +30,58 @@ app.listen(8080, () => {
 
 //todos 전체조회
 app.get("/posts", (req, res) => {
-  res.send("전체조회");
-  console.log("전체 조회 성공");
+  const query = "SELECT * FROM TODOS";
+
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      res.status(500).send("Error retrieving todos");
+      console.error(error);
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
 //todo 상세조회
 app.get(`/posts/detail/:id`, (req, res) => {
   const id = req.params.id;
-  res.send("전체조회");
+  const query = "SELECT * FROM TODOS WHERE id = ?";
+
+  connection.query(query, [id], (error, results, fields) => {
+    if (error) {
+      res.status(500).send("Error retrieving todo");
+      console.error(error);
+    } else {
+      if (results.length === 0) {
+        res.status(404).send("Todo not found");
+      } else {
+        res.status(200).json(results[0]);
+      }
+    }
+  });
 });
 
 //todo 추가
 app.post("/posts", (req, res) => {
-  console.log("hihi");
-  // const params = req.params.id;
-  // console.log(params);
-  res.send("<h1>Successfully added!</h1>");
+  const { id, title, text, date, done } = req.body;
+  const query =
+    "INSERT INTO TODOS (id, title, text, date, done) VALUES (?, ?, ?, ?, ?)";
+
+  connection.query(
+    query,
+    [id, title, text, date, done],
+    (error, results, fields) => {
+      if (error) {
+        res.status(500).send("Error adding new todo");
+        console.error(error);
+      } else {
+        res.status(201).send({
+          message: "Successfully added!",
+          insertedId: results.insertId,
+        });
+      }
+    },
+  );
 });
 
 //todo 수정
