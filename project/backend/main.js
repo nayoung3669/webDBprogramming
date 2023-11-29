740b1dbbbb0d27a38299aff08d2c4816a820854e
nayoung3669 <skdud3669@gmail.com>
2023-11-29 13:52:32 +0900

    node 세션 추가

--- project/backend/main.js
+++ project/backend/main.js
@@ -61,6 +61,21 @@ app.get(`/posts/detail/:id`, (req, res) => {
   });
 });
 
+// TODOS를 날짜별로 조회
+app.get("/posts/date/:date", (req, res) => {
+  const date = req.params.date;
+  const query = "SELECT * FROM TODOS WHERE DATE(date) = ?";
+
+  connection.query(query, [date], (error, results, fields) => {
+    if (error) {
+      res.status(500).send("Error retrieving todos by date");
+      console.error(error);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
 //todo 추가
 app.post("/posts", (req, res) => {
   const { id, title, text, date, done } = req.body;
@@ -87,11 +102,61 @@ app.post("/posts", (req, res) => {
 //todo 수정
 app.put(`/posts/:id`, (req, res) => {
   const id = req.params.id;
-  res.send("<h1>Successfully updated!</h1>");
+  const { done } = req.body;
+
+  const query = "UPDATE TODOS SET done = ? WHERE id = ?";
+
+  connection.query(query, [done, id], (error, results, fields) => {
+    if (error) {
+      res.status(500).send("Error updating todo");
+      console.error(error);
+    } else {
+      res.status(200).send("Successfully updated!");
+    }
+  });
 });
 
 //todo 삭제
 app.delete(`/posts/:id`, (req, res) => {
   const id = req.params.id;
-  res.send("<h1>Successfully deleted!</h1>");
+  const query = "DELETE FROM TODOS WHERE id = ?";
+
+  connection.query(query, [id], (error, results, fields) => {
+    if (error) {
+      res.status(500).send("Error deleting todo");
+      console.error(error);
+    } else {
+      res.status(200).send("Successfully deleted!");
+    }
+  });
+});
+
+//로그인
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const query = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+  connection.query(query, [username, password], (error, results, fields) => {
+    if (error) {
+      res.status(500).send("Error retrieving user");
+      console.error(error);
+    } else {
+      if (results.length === 1) {
+        req.session.user = results[0];
+
+        res.status(200).json({ message: "Login successful", user: results[0] });
+      } else {
+        // 로그인 실패
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    }
+  });
+});
+
+// 로그아웃
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+
+  res.status(200).json({ message: "Logout successful" });
 });
